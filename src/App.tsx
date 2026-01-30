1b5d1988719dbcd8f167fa74e488fcaf6ae079c2
Shubhang Gupta <guptashubhang2000@gmail.com>
2026-01-30 18:30:39 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { GitHubBanner, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -6,12 +6,21 @@ import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
+import { BookOpen, GraduationCap, Home } from "lucide-react";
+import { Layout } from "./components/refine-ui/layout/layout";
+import SubjectsList from "@/pages/subjects/list.tsx";
+import SubjectsCreate from "@/pages/subjects/create.tsx";
+import Dashboard from "./pages/dashboard";
+
+// import ClassesList from "@/pages/classes/list.tsx";
+// import ClassesCreate from "@/pages/classes/create.tsx";
+// import ClassesShow from "@/pages/classes/show.tsx";
 
 function App() {
   return (
@@ -29,9 +38,48 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "uGWMyL-ZBafbC-9U4OP5",
               }}
+              resources={[
+                {
+                  name: "dashboard",
+                  list: "/",
+                  meta: { label: "Home", icon: <Home /> },
+                },
+                {
+                  name: "subjects",
+                  list: "/subjects",
+                  create: "/subjects/create",
+                  meta: { label: "Subjects", icon: <BookOpen /> },
+                },
+                {
+                  name: "classes",
+                  list: "/classes",
+                  create: "/classes/create",
+                  show: "/classes/show/:id",
+                  meta: { label: "Classes", icon: <GraduationCap /> },
+                },
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route
+                  element={
+                    <Layout>
+                      <Outlet />
+                    </Layout>
+                  }
+                >
+                  <Route path="/" element={<Dashboard />} />
+
+                  <Route path="subjects">
+                    <Route index element={<SubjectsList />} />
+                    <Route path="create" element={<SubjectsCreate />} />
+                  </Route>
+
+                  {/* <Route path="classes">
+                    <Route index element={<ClassesList />} />
+                    <Route path="create" element={<ClassesCreate />} />
+                    <Route path="show/:id" element={<ClassesShow />} />
+                  </Route> */}
+                </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
---
+++ src/pages/classes/list.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ClassesList = () => {
+  return (
+    <div>ClassesList</div>
+  )
+}
+
+export default ClassesList
